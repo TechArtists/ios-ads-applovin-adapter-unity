d6aa53a0512bd9fe672a3d3f90edf4d8462606b4
Tataru Robert <robert.tataru98@gmail.com>
2025-03-17 18:14:02 +0200

    First commit

---
+++ Frameworks/UnityAppLovinAdapter.xcframework/ios-arm64/UnityAppLovinAdapterFramework.framework/Headers/UnityAppLovinAdapterFramework.h
@@ -0,0 +1,18 @@
+//
+//  UnityAppLovinAdapterFramework.h
+//  UnityAppLovinAdapterFramework
+//
+//  Created by Robert Tataru on 10.03.2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnityAppLovinAdapterFramework.
+FOUNDATION_EXPORT double UnityAppLovinAdapterFrameworkVersionNumber;
+
+//! Project version string for UnityAppLovinAdapterFramework.
+FOUNDATION_EXPORT const unsigned char UnityAppLovinAdapterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnityAppLovinAdapterFramework/PublicHeader.h>
+
+
